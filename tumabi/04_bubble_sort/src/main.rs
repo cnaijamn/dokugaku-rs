d7a3c6dc234e9174e4d2ebca26d4b85850561146
cnaijamn <cnaijamn@gmail.com>
2026-04-20 14:49:02 +0900

    追加

---
+++ tumabi/04_bubble_sort/src/main.rs
@@ -0,0 +1,67 @@
+// https://www.youtube.com/watch?v=sTVNP5q-fMQ
+
+fn bubble_sort(v: &mut Vec<i32>, compar: fn(i32, i32) -> bool) {
+    let mut flag = true;
+    let mut k = 0;
+
+    while flag {
+        flag = false;
+        let mut i = 0;
+
+        while i < v.len() - 1 - k {
+            if compar(v[i], v[i + 1]) {
+                let tmp = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = tmp;
+                flag = true;
+            }
+
+            i += 1;
+        }
+
+        k += 1;
+    }    
+}
+
+fn desc(a: i32, b: i32) -> bool {
+    a < b
+}
+
+fn asc(a: i32, b:i32) -> bool {
+    a > b
+}
+
+fn show_vec(v: &Vec<i32>) {
+    for el in v.iter() {
+        println!("{}", el);
+    }
+}
+
+fn line(s: &str, n: i32) {
+    for _ in 0..n {
+        print!("{}", s);
+    }
+    println!("");
+}
+
+fn main() {
+    let mut v = vec![5, 2, 3, 1, 4];
+
+    bubble_sort(&mut v, desc);
+    show_vec(&v);
+    //-> 5
+    //   4
+    //   3
+    //   2
+    //   1
+
+    line("-", 40);
+
+    bubble_sort(&mut v, asc);
+    show_vec(&v);
+    //-> 1
+    //   2
+    //   3
+    //   4
+    //   5
+}
---
+++ tumabi/05_rust_cast/src/main.rs
@@ -0,0 +1,73 @@
+// https://www.youtube.com/watch?v=_-o1NQx_DCE
+// キャスト(型変換)
+
+fn main() {
+    // 整数型でキャスト
+    test_int();
+
+    // 構造体でキャスト
+    test_struct();
+}
+
+fn test_int() {
+    let a: i32 = 100;
+    let b: u32 = a as u32;
+    println!("{}", b);
+    //-> 100
+
+    let a: i32 = -100;
+    let b: u32 = a as u32;
+    println!("{}", b);
+    //-> 4294967196
+    // まずい
+
+    // 8bit -> 16bit
+    let a: i8 = 8;
+    let b: i16 = a as i16;
+    println!("{}", b);
+    //-> 8
+
+    // 16bit -> 8bit
+    let a: i16 = 8;
+    let b: i8 = a as i8;
+    println!("{}", b);
+    //-> 8
+
+    // 二進数を {:b} で表示出力が可能
+
+    let a: i16 = 30000;
+    let b: i8 = a as i8;
+    println!("{} (a:{:b} b:{:b})", b, a, b);
+    //-> 48 (a:111010100110000 b:110000)
+    // まずい
+}
+
+////////////////////////////////////////////////
+
+// From / Into
+// 特定の値を構造体に変換する
+use std::convert::{ From, Into };
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i16> for Number {
+    fn from(x: i16) -> Self {
+        Number {
+            value: x as i32,
+        }
+    }
+}
+
+fn test_struct() {
+    let a: i16 = 8;
+    let num1: Number = Number::from(a);
+    let num2: Number = a.into();
+
+    println!("num1: {:?}", num1);
+    //-> num1: Number { value: 8 }
+    println!("num2: {:?}", num2);
+    //-> num1: Number { value: 8 }
+}
