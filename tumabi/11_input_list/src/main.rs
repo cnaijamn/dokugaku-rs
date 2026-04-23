55496dd8011cfdd62297f19c9f7d0f4ec65da079
cnaijamn <cnaijamn@gmail.com>
2026-04-23 14:45:03 +0900

    追加

---
+++ tumabi/11_input_list/src/main.rs
@@ -0,0 +1,48 @@
+// https://www.youtube.com/watch?v=0eSBBSp-KGk
+
+//use std::{collections::LinkedList, io::Write};
+use std::collections::LinkedList;
+use std::io::Write;
+
+fn main() {
+    let mut list = LinkedList::<String>::new();
+
+    loop {
+        let mut line = String::new();
+
+        print!("input > ");
+        std::io::stdout().flush().unwrap();
+
+        match std::io::stdin().read_line(&mut line) {
+            Ok(size) => {
+                if size <= 0 { //バグ？:エンターのみでもlineに改行が残る(sizeがゼロにはならない)
+                    break;
+                }
+            }
+            Err(err) => {
+                eprint!("{}", err);
+                std::process::exit(1);
+            }
+        }
+
+        let args: Vec<&str> = line.trim().split(" ").collect();
+        if args[0] == "show" {
+            for item in list.iter() {
+                print!("{} ", item);
+            }
+            println!("");
+        } else if args[0] == "push_back" {
+            list.push_back(args[1].to_string());
+        } else if args[0] == "pop_back" {
+            let item = list.pop_back();
+            if !item.is_none() {
+                println!("{}", item.unwrap());
+            }
+        } else if args[0] == "pop_front" {
+            let item = list.pop_front();
+            if !item.is_none() {
+                println!("{}", item.unwrap());
+            }
+        }
+    }
+}
