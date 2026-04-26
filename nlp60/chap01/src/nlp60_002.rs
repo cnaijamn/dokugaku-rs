01a900c47bb3f32bc47682140e5ba1aff04fb509
Chez Naijamn <cnaijamn@gmail.com>
2026-04-26 17:59:33 +0900

    修正

--- nlp60/chap01/src/nlp60_002.rs
+++ nlp60/chap01/src/nlp60_002.rs
@@ -1,9 +1,12 @@
-use rand::Rng;
+//use rand::Rng;
+use rand::RngExt;
 
 fn main() {
-    let fortunes = ["大吉", "忠吉", "凶"];
-    let mut rng = rand::thread_rng();
-    let index = rng.gen_range(0..fortunes.len());
+    let fortunes = ["大吉", "中吉", "凶"];
+    //let mut rng = rand::thread_rng();
+    //let index = rng.gen_range(0..fortunes.len());
+    let mut rng = rand::rng();
+    let index = rng.random_range(0..fortunes.len());
 
     println!("今日の運勢は... {} です", fortunes[index]);
 }
